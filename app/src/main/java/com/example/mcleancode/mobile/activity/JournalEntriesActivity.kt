70043442a93ab2fd1bbf762e9d90e88240c4807e
Chris McLean <havok2905@gmail.com>
2017-08-20 14:43:59 -0400

    added functionless activities for journal entries, menu, and the case file

---
+++ app/src/main/java/com/example/mcleancode/mobile/activity/JournalEntriesActivity.kt
@@ -0,0 +1,14 @@
+package com.example.mcleancode.mobile.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.example.mcleancode.mobile.R
+
+class JournalEntriesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_journal_entries)
+    }
+}
